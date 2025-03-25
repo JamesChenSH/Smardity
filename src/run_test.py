98559784f8b7ca107cd1de2b65abcacee73df848
pco111 <15262555+pco111@user.noreply.gitee.com>
2025-03-25 18:06:34 -0400

    Update file with new content

--- src/run_test.py
+++ src/run_test.py
@@ -6,13 +6,19 @@ from process_data import SmardityDataset
 
 from train import evaluate, collate
 
-DATA_PATH = "../data/test"
+DATA_PATH = "data/test"    # TODO: may need to update this to match your local env
+if torch.cuda.is_available():
+    DEVICE = "cuda"
+elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+    DEVICE = "mps"
+else:
+    DEVICE = "cpu"
 
 if __name__ == '__main__':
     # Load the tokenizer
     tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
     # Load the model
-    model = RobertaForSequenceClassification.from_pretrained("models/CodeBERT-solidifi_finetuned").to('cuda')
+    model = RobertaForSequenceClassification.from_pretrained("models/CodeBERT-solidifi_finetuned").to(DEVICE)    # TODO: use fine-tuned model instead
     # Load the dataset
     if os.path.exists(DATA_PATH + "/dataset.pt"):
         dataset = torch.load(DATA_PATH + "/dataset.pt", weights_only=False)
@@ -22,4 +28,4 @@ if __name__ == '__main__':
     # Create the dataloader
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, collate_fn=collate)
     # Evaluate the model
-    evaluate(model, dataloader, 'cuda')
+    evaluate(model, dataloader, DEVICE)
